37bd38ddcbb01ee2d0542d7df4893cfebffd88d2
Nelson Moreno <thegreyabove@gmail.com>
2024-12-18 15:28:24 -0500

    DOM lab create commit 1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let mainTag = document.querySelector('#main')
+mainTag.remove()
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Nelson Moreno is the champion"
+document.body.append("h1")
+
+
+
